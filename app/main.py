44c7fcaf83b4f845e5cbe138bcaafc8a43e6d4e6
USER <USER@MacBook-Air-4.local>
2025-06-29 10:07:56 +0530

    cold-email-generator-tool

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from utils import clean_text
 
 def create_streamlit_app(llm, portfolio, clean_text):
     st.title("📧 Cold Mail Generator")
-    url_input = st.text_input("Enter a URL:", value="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")
+    url_input = st.text_input("Enter a URL:", value="GROQ_API_KEY")
     submit_button = st.button("Submit")
 
     if submit_button:
