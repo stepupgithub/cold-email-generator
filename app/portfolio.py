84534d314262a8152f585dad7ce08f065d64ec14
USER <USER@MacBook-Air-4.local>
2025-06-29 11:58:11 +0530

    cold-email-generator-tool

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,11 +1,13 @@
 import pandas as pd
-import chromadb
+
 import uuid
 
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import sqlite3
+
+import chromadb
 class Portfolio:
     def __init__(self, file_path="app/resource/my_portfolio.csv"):
         self.file_path = file_path
